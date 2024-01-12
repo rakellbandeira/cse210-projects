4db0e4a5c3e1f74980f69b5c749f6bb4b6ca6f69
rakellbandeira <per22016l@byui.edu>
2024-01-12 13:40:49 -0400

    Learning Actv - If statements + char/string/parse

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+
+        Console.Write("Hello, what is your grade percentage?");
+        int gradePercent = int.Parse(Console.ReadLine());
+        char letter;
+        
+
+        if (gradePercent < 60)
+        {
+            
+            letter = 'F';
+        }
+
+        else if (gradePercent >= 60)
+        {
+            
+            letter = 'D';
+        }
+
+        else if (gradePercent >=70)
+        {
+            
+            letter = 'C';
+        }
+
+        else if (gradePercent >=80)
+        {
+            
+            letter = 'B';
+        }
+
+        else
+        {
+            
+            letter = 'A';
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (gradePercent >=70)
+        {
+            Console.WriteLine("Congratulations! You passed this class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry. Your grades are too low to pass, but you can try again next time!");
+        }
+
+
     }
 }
