b25918cda2b6c829f7459723c89b1b08ab8d886c
rakellbandeira <per22016l@byui.edu>
2024-01-13 19:10:31 -0400

    Week 1 Learning Activ 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Getting a random Number
+        Random random = new Random();
+        int randomNumber = random.Next(1, 99);
+        Console.WriteLine($"The random number: {randomNumber}");
+
+        int userGuess;
+
+        do
+        {
+            Console.Write("Enter your guess (Up to 2 dig): ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out userGuess))
+            {
+                if (userGuess == randomNumber)
+                {
+                    Console.Write("You guessed right!");
+                }
+                else if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Try higher!");
+                }
+                else
+                {
+                    Console.WriteLine("Try lower!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+        } while (userGuess != randomNumber);
     }
 }
+
+
+
+
